5ffb12aa9562f8926d9304dfb2cabe3b08a1a7c8
sergey <sbarchshevskiy@gmail.com>
2021-02-02 21:13:14 +0000

    added new exercises

---
+++ take2/w1/d2-focal/diceRoller.js
@@ -0,0 +1,14 @@
+const args = process.argv[2];
+
+const rollDice = function(rolls) {
+ 
+  let diceRoll = '';
+
+  for (let i = 0; i < rolls; i++) {
+    diceRoll += Math.floor(Math.random() * 6) + 1 + ' : ';
+
+  }
+  return `you rolled ${rolls} timmes, result: ${diceRoll}`;
+};
+
+console.log(rollDice(args));
