366874f5cd1370e98fd85d8a971bc1a73b7271ce
sergey <sbarchshevskiy@gmail.com>
2021-02-02 21:32:45 +0000

    corrected average/reverse errors

--- take2/w1/d2-focal/diceRoller.js
+++ take2/w1/d2-focal/diceRoller.js
@@ -5,10 +5,11 @@ const rollDice = function(rolls) {
   let diceRoll = '';
 
   for (let i = 0; i < rolls; i++) {
-    diceRoll += Math.floor(Math.random() * 6) + 1 + ' : ';
+    diceRoll += Math.floor(Math.random() * 6) + 1 + '  ';
 
   }
   return `you rolled ${rolls} timmes, result: ${diceRoll}`;
 };
 
 console.log(rollDice(args));
+
---
+++ take2/w1/d2-focal/test.js
@@ -0,0 +1,11 @@
+let input = process.argv[2];
+
+const reverse = function(original) {
+  return original.split('').reverse().join('');
+};
+
+
+if (input){
+  console.log(reverse(input));
+}
+
